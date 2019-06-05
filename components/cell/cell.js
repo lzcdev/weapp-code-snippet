b916b82eb08b4e6c66e010726353c9cee1fa5541
lzcdev <lzc2134@gmail.com>
2019-06-05 10:34:52 +0800

    添加cell组件

---
+++ components/cell/cell.js
@@ -0,0 +1,93 @@
+// components/cell/cell.js
+Component({
+  relations: {
+    '../cell-group/cell-group': {
+      type: 'parent'
+    }
+  },
+  options: {
+    multipleSlots: true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 是否显示菜单箭头，默认不显示
+    menuArrow: {
+      type: Boolean,
+      value: ''
+    },
+    // 左侧标题
+    title: {
+      type: String,
+      value: ''
+    },
+    // 标题下方描述信息
+    label: {
+      type: String,
+      value: ''
+    },
+    // 右侧内容
+    value: {
+      type: String,
+      value: ''
+    },
+    // 链接类型，可选值为 navigateTo, redirectTo, switchTab, relaunch
+    linkType: {
+      type: String,
+      value: 'navigateTo'
+    },
+    appId: {
+      type: String,
+      value: ''
+    },
+    extraData: {
+      type: Object,
+      value: {}
+    },
+    url: {
+      type: String,
+      value: ''
+    }
+
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    isLastCell: true
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    navigateTo() {
+      let url = this.data.url;
+      this.triggerEvent('click');
+      if (!url || url === 'true' || url === 'false') return;
+
+      if (['navigateTo', 'redirectTo', 'switchTab', 'reLaunch', 'navigateToMiniProgram'].indexOf(this.data.linkType) === -1) {
+        return;
+      }
+      if (this.data.linkType == 'navigateToMiniProgram') {
+        wx.navigateToMiniProgram({
+          appId: this.data.appId,
+          path: this.data.url,
+          extraData: this.data.extraData
+        })
+        return;
+      }
+      wx[this.data.linkType].call(wx, {
+        url
+      });
+    },
+    updateIsLastCell(isLastCell) {
+      this.setData({
+        isLastCell
+      });
+    }
+  }
+})
